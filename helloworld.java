689c72842d57946c3ec6813101fe09a723bb1a07
hhshang <329123463@qq.com>
2015-10-15 20:05:14 +0800

    helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@ package learnjava;
 
 public class helloworld {
 	public static void main(String args[]){
-		System.out.println("Today is a sunnt day");
+		System.out.println("hello world");
 		System.out.print("huan");
 	}
 }
